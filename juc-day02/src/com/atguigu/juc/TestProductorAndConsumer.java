97387c5ee98f7b2bf9e822e04c4187475069bb43
wlh <1101983713@qq.com>
2017-07-20 23:49:15 +0800

    JUC day2

---
+++ juc-day02/src/com/atguigu/juc/TestProductorAndConsumer.java
@@ -0,0 +1,103 @@
+package com.atguigu.juc;
+
+/*
+ * 生产者消费者案例：
+ */
+public class TestProductorAndConsumer {
+
+	public static void main(String[] args) {
+		Clerk clerk = new Clerk();
+
+		Productor pro = new Productor(clerk);
+		Consumer con = new Consumer(clerk);
+
+		new Thread(pro, "生产者 A").start();
+		new Thread(con, "消费者 B").start();
+		
+
+		new Thread(pro, "生产者 C").start();
+		new Thread(con, "消费者 D").start();
+	}
+
+}
+
+/*class Clerk {
+	private int product = 0;
+
+	// 进货
+	public synchronized void get() {
+		while (product >= 1) { //为了避免虚假唤醒，应该总是使用在循环中。
+			System.out.println("产品已满！");
+
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+			}
+
+		}
+
+		System.out
+				.println(Thread.currentThread().getName() + " : " + ++product);
+
+		this.notifyAll();
+
+	}
+
+	// 卖货
+	public synchronized void sale() {
+		while (product <= 0) {
+			System.out.println("缺货！");
+
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		
+		System.out
+				.println(Thread.currentThread().getName() + " : " + --product);
+
+		this.notifyAll();
+	}
+}
+
+// 生产者
+class Productor implements Runnable {
+
+	private Clerk clerk;
+
+	public Productor(Clerk clerk) {
+		this.clerk = clerk;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 20; i++) {
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			clerk.get();
+		}
+	}
+}
+
+// 消费者
+class Consumer implements Runnable {
+
+	private Clerk clerk;
+
+	public Consumer(Clerk clerk) {
+		this.clerk = clerk;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 20; i++) {
+			clerk.sale();
+		}
+	}
+
+}*/
